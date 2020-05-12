9a07914ae795419a8766039c4438f200aba50c13
Shona <shona@gmail.com>
2020-05-12 22:50:06 +0530

    spacing

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,8 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const msgOne = document.querySelector('#msg-1');
 const msgTwo = document.querySelector('#msg-2');
+
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
